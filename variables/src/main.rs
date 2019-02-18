91a6b8166516d6f5c11e3d25c2f3cd5ad8319194
gaurav <sgauravs@gmail.com>
2019-02-18 22:42:02 +0000

    variables demo

---
+++ variables/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, Rust Variables!");
+    let name = "Gaurav";
+    println!("Name is {}", name);
+    println!("Trying to change name...");
+    // name = "Dublin"; This will result in compiler error
+    let mut age = 23;
+    println!("Age is {}", age);
+    println!("Changing age to 24");
+    age = 24;
+    if age == 24 {
+        println!("Age changed successfully");
+    } else {
+        println!("Age could not be changed");
+    }
+}
